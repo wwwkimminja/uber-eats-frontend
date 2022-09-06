32acd566f25e0cc41d5bb1b750f7f825643ff658
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-09-07 01:43:10 +0900

    useReactiveVar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { gql, useQuery } from '@apollo/client';
-import React from 'react';
+import { useReactiveVar } from '@apollo/client';
+import { isLoggedInVar } from './apollo';
 import { LoggedInRouter } from './routers/logged-in-router';
 import { LoggedOutRouter } from './routers/logged-out-router';
 
-const IS_LOGGED_IN=gql`
-  query isLoggedIn{
-    isLoggedIn @client
-  }
-`
+
+
 function App() {
-  const {data:{isLoggedIn}}=useQuery(IS_LOGGED_IN);
-  
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return isLoggedIn?<LoggedInRouter/> :<LoggedOutRouter/>
 }
 
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,5 +1,10 @@
+import { isLoggedInVar } from "../apollo";
 
 
-export const LoggedInRouter =()=><span>Logged in</span>
-
+export const LoggedInRouter =()=>(
+    <div>
+    <h1>Logged in</h1>
+    <button onClick={()=>isLoggedInVar(false)}>Log Out</button>
+    </div>
+)
 
