6b257176911e0aa31645e3b2a0617f2ac8ce2f77
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-09-12 21:21:03 +0900

    Route

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,43 +1,16 @@
-import { useForm } from "react-hook-form";
-
-interface IForm {
-    email:string;
-    password:string;
-}
+import { BrowserRouter,Route,Routes} from "react-router-dom";
+import CreateAccount from "../pages/create-account";
+import Login from "../pages/login";
 
 export const LoggedOutRouter =()=>{
-    const {register,watch,handleSubmit,formState:{errors}} = useForm<IForm>();
-    const onSubmit=()=>{
-        console.log(watch("email"));
-    };
-    const onInvalid=()=>{
-        console.log("cant create account");
-    }
-    console.log()
-
     return(
-    <div>
-    <h1>Logged out</h1>
-    <form onSubmit={handleSubmit(onSubmit,onInvalid)}>
-        <div>
-         <input {...register("email", {
-            required: "This is required",
-            pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
-            })}
-            name="email"
-            type="email"
-            placeholder="email"
-            />
-            {errors.email?.message &&(<span className="font-bold text-red-600">{errors.email?.message}</span>)}
-            {errors.email?.type ==="pattern" &&(<span className="font-bold text-red-600">Only gmail allowed</span>)}
-        </div>
-        <div>
-        <input {...register("password", { required: true })}/>
-        </div>
-        <button className="bg-yellow-300 text-white">Submit</button>
-    </form>
-   
-    </div>
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login/>}/>
+            <Route path="/create-account" element={<CreateAccount/>}/>
+        </Routes>
+        </BrowserRouter>
+
     );
 };
 
