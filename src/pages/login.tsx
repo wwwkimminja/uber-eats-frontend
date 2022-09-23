f5486cd595682d488aa37aa7e65187fed7f46f54
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-09-23 17:21:04 +0900

    form Login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
+interface ILoginForm{
+    email?:string;
+    password?:string;
+}
 function Login() {
+    const {register,getValues,handleSubmit,formState:{errors}}= useForm<ILoginForm>();
+    const onSubmit=()=>{
+        console.log(getValues())
+    }
   return (
     <div className='h-screen flex items-center justify-center bg-gray-800'>
-        <div className='bg-white w-full max-w-lg py-10 rounded-lg text-center'>
+        <div className='bg-white w-full max-w-lg pt-8 pb-7 rounded-lg text-center'>
             <h3 className='font-bold text-lg text-gray-800'>Log in</h3>
-            <form className='flex flex-col mt-5 px-5'>
-                <input className='bg-gray-100 shadow-inner focus:outline-none  border-2 focus:border-opacity-50 focus:border-green-600  mb-3 py-3 px-5 rounded-lg'placeholder='Email'/>
-                <input className='bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 py-3 px-5 rounded-lg'placeholder='Password'/>
-                <button className='py-3 px-5 bg-gray-800 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90'>Login</button>
+            <form onSubmit={handleSubmit(onSubmit)}className='grid gap-5 mt-5 px-5'>
+                <input {...register("email",{required:"Email is required",minLength:10})} name="email"type="email" className="input mb-3"placeholder='Email'/>
+                <span>{errors.email?.message}</span>
+                <input {...register("password",{required:"Password is required"})} name="password"type="password" className="input"placeholder='Password'/>
+                <button className='btn mt-3'>Login</button>
             </form>
         </div>
     </div>
