e65516902d937a190b4210b1f514a5a9b8d0c98c
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-10-08 17:35:04 +0900

    using Helmet

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,6 @@
 import { gql, useMutation } from '@apollo/client';
 import React from 'react'
+import Helmet from 'react-helmet';
 import { useForm } from 'react-hook-form';
 import { FormError } from '../components/form-error';
 import { LoginMutation, LoginMutationVariables } from '../__generated__/LoginMutation';
@@ -54,6 +55,9 @@ function Login() {
     }
   return (
     <div className='h-screen flex items-center flex-col mt-10 lg:mt-28 m-6'>
+        <Helmet>
+        <title>Login | Uber Eats</title>
+        </Helmet>
         <div className="w-full max-w-screen-sm flex flex-col items-center">
        <img src={uberLogo} className='w-52 mb-5' alt="uber eats logo"/>
        <h4 className='w-full font-medium text-left text-3xl mb-5'>Welcome back</h4>
@@ -63,11 +67,11 @@ function Login() {
                 <input {...register("password",{required:"Password is required",minLength:10})} name="password"type="password" className='input' placeholder='Password'/>
                 {errors.password?.message&& <FormError errorMessage={errors.password?.message}/>}
                 {errors.password?.type==="minLength"&& <FormError errorMessage="Password must be than 10 chars"/>}
-              <Button canClick={isValid} loading={loading} actionText={"Log in"}/>
+              <Button canClick={isValid} loading={loading} actionText={"Create Account"}/>
                 {loginMutationResult?.login.error && <FormError errorMessage={loginMutationResult.login.error}/>}
             </form>
             <div>
-                New to Uber? <Link to="/create-account" className=" text-lime-600 hover:underline">Create an Account</Link>
+                New to Uber? <Link to="/create-account" className=" text-lime-600 hover:underline ">Create an Account</Link>
             </div>
         </div>
         </div>
