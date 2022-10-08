b5ad2c81588377e0c0b0e96f0d6b437c1c755345
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-10-08 16:07:03 +0900

    create Button

---
+++ src/components/button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+interface IButtonProps {
+    canClick:boolean;
+    loading:boolean;
+    actionText:string;
+}
+
+export const Button: React.FC<IButtonProps>=({canClick,loading,actionText})=> {
+  return (
+    <button className={
+        `text-lg font-medium focus:outline-none text-white py-4  transition-colors mb-3
+        ${canClick ?  "bg-lime-600 hover:bg-lime-700":"bg-gray-300 pointer-events-none"}`}>{loading ?"Loading...":"Log In"}</button>
+  )
+}
+
